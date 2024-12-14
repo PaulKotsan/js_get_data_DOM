705d92e8d7772f0b2ef3dd4930dae2b27e039d4f
PaulKotsan <pavlokotsan421@gmail.com>
2024-12-14 12:14:26 -0800

    Fixes Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,14 @@
 // write your code here
 const populationCounter = document.querySelectorAll('.population');
 const populationNumbers = [...populationCounter].map((node) =>
-  Number(node.textContent.replace(/,/g, '')));
+  Number(node.textContent.replace(/,/g, '')),
+);
 
 const total = populationNumbers.reduce((sum, num) => sum + num, 0);
 const average = total / populationNumbers.length;
 
-document.querySelector('.total-population').textContent = total;
+document.querySelector('.total-population').textContent =
+  total.toLocaleString();
 
-document.querySelector('.average-population').textContent = average.toFixed(0);
+document.querySelector('.average-population').textContent =
+  average.toLocaleString();
